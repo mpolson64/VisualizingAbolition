21fa036f8f00791d6d7e68215b85142a8d8b9fd4
Miles Olson <mpolson64@gmail.com>
2019-04-01 16:58:02 -0500

    Tooltips now

--- src/viz/pie.js
+++ src/viz/pie.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-underscore-dangle, func-names */
 import * as d3 from 'd3';
+import d3Tip from 'd3-tip';
 
 import { histogram, coalesceHistogram } from '../util';
 
@@ -13,7 +14,6 @@ let color;
 
 let pie;
 let arc;
-let outerArc;
 
 let key;
 
@@ -29,8 +29,6 @@ const mergeWithFirstEqualZero = (first, second) => {
   return d3.merge([second, onlyFirst]).sort((a, b) => d3.ascending(a.key, b.key));
 };
 
-const midAngle = d => d.startAngle + (d.endAngle - d.startAngle) / 2;
-
 const update = (data, filteredData) => {
   const counts = coalesceHistogram(histogram(data, filteredData, 'Occupation', x => x));
 
@@ -43,6 +41,14 @@ const update = (data, filteredData) => {
   if (data0.length === 0) data0 = counts;
   const was = mergeWithFirstEqualZero(counts, data0);
   const is = mergeWithFirstEqualZero(data0, counts);
+
+  /* -------- TIP -------------*/
+  const tip = d3Tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(d => `${d.data.key}: <span style='color:red'>${d.data.value}</span>`);
+
+  svg.call(tip);
 
   /* ------- SLICE ARCS -------*/
 
@@ -80,7 +86,9 @@ const update = (data, filteredData) => {
   slice = svg
     .select('.slices')
     .selectAll('path.slice')
-    .data(pie(counts), key);
+    .data(pie(counts), key)
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
 
   slice
     .exit()
@@ -88,115 +96,9 @@ const update = (data, filteredData) => {
     .delay(duration)
     .duration(0)
     .remove();
-
-  /* ------- TEXT LABELS -------*/
-
-  let text = svg
-    .select('.labels')
-    .selectAll('text')
-    .data(pie(was), key);
-
-  text
-    .enter()
-    .append('text')
-    .attr('dy', '.35em')
-    .style('opacity', 0)
-    .text(d => d.data.key)
-    .each(function (d) {
-      this._current = d;
-    });
-
-  text = svg
-    .select('.labels')
-    .selectAll('text')
-    .data(pie(is), key);
-
-  text
-    .transition()
-    .duration(duration)
-    .style('opacity', d => (d.data.value === 0 ? 0 : 1))
-    .attrTween('transform', function (d) {
-      const interpolate = d3.interpolate(this._current, d);
-      const _this = this;
-      return function (t) {
-        const d2 = interpolate(t);
-        _this._current = d2;
-        const pos = outerArc.centroid(d2);
-        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-        return `translate(${pos})`;
-      };
-    })
-    .styleTween('text-anchor', function (d) {
-      const interpolate = d3.interpolate(this._current, d);
-      return function (t) {
-        const d2 = interpolate(t);
-        return midAngle(d2) < Math.PI ? 'start' : 'end';
-      };
-    });
-
-  text = svg
-    .select('.labels')
-    .selectAll('text')
-    .data(pie(counts), key);
-
-  text
-    .exit()
-    .transition()
-    .delay(duration)
-    .remove();
-
-  /* ------- SLICE TO TEXT POLYLINES -------*/
-
-  let polyline = svg
-    .select('.lines')
-    .selectAll('polyline')
-    .data(pie(was), key);
-
-  polyline
-    .enter()
-    .append('polyline')
-    .style('opacity', 0)
-    .each(function (d) {
-      this._current = d;
-    });
-
-  polyline = svg
-    .select('.lines')
-    .selectAll('polyline')
-    .data(pie(is), key);
-
-  polyline
-    .transition()
-    .duration(duration)
-    .style('opacity', d => (d.data.value === 0 ? 0 : 1))
-    .attrTween('points', function (d) {
-      this._current = this._current;
-      const interpolate = d3.interpolate(this._current, d);
-      const _this = this;
-      return function (t) {
-        const d2 = interpolate(t);
-        _this._current = d2;
-        const pos = outerArc.centroid(d2);
-        pos[0] = radius * 1 * (midAngle(d2) < Math.PI ? 1 : -1);
-        return [arc.centroid(d2), outerArc.centroid(d2), pos];
-      };
-    });
-
-  polyline = svg
-    .select('.lines')
-    .selectAll('polyline')
-    .data(pie(counts), key);
-
-  polyline
-    .exit()
-    .transition()
-    .delay(duration)
-    .remove();
 };
 
 const init = (data, filteredData) => {
-  const counts = coalesceHistogram(histogram(data, filteredData, 'Occupation', x => x));
-
   svg = d3
     .select('#pieChart')
     .append('svg')
@@ -220,17 +122,11 @@ const init = (data, filteredData) => {
     .outerRadius(radius * 1.0)
     .innerRadius(radius * 0.4);
 
-  outerArc = d3
-    .arc()
-    .innerRadius(radius * 0.5)
-    .outerRadius(radius * 1);
-
   svg.attr('transform', `translate(${width / 2}, ${height / 2})`);
 
   key = d => d.data.key;
 
-  color = d3.scaleOrdinal(d3.schemeCategory10)
-    .domain(counts.map(pair => pair.key));
+  color = d3.scaleOrdinal(d3.schemeCategory10);
 
   update(data, filteredData);
 };
