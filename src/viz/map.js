0303a2a8b316c05eabb13d270c5cdfd27d9ac80b
Miles Olson <mpolson64@gmail.com>
2019-04-08 00:05:51 -0500

    Eslint

--- src/viz/map.js
+++ src/viz/map.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import * as d3v4 from 'd3';
 import * as Datamap from 'datamaps';
 import { histogram } from '../util';
@@ -6,62 +5,22 @@ import { histogram } from '../util';
 // TODO: get rid of this lookup table
 // Note that coordinates are [lat, lon]
 const coordinates = {
-  'Bazaruto': [-21.64915, 35.46723],
+  Bazaruto: [-21.64915, 35.46723],
   'Cabo Delgado': [-12.33761, 40.59913],
-  'Inhambane': [-23.87328, 35.4103],
+  Inhambane: [-23.87328, 35.4103],
   'Lourenço Marques': [-25.96924, 32.57317],
-  'Moçambique': [-15.0359, 40.73486],
-  'Quelimane': [-17.87368, 36.88194],
-  'Sena': [-17.4524, 35.0333],
-  'Sofala': [-20.02436, 34.74092],
-  'Tete': [-16.13281, 33.60638],
-  'Sancul': [-15.07676, 40.70331],
+  Moçambique: [-15.0359, 40.73486],
+  Quelimane: [-17.87368, 36.88194],
+  Sena: [-17.4524, 35.0333],
+  Sofala: [-20.02436, 34.74092],
+  Tete: [-16.13281, 33.60638],
+  Sancul: [-15.07676, 40.70331],
 };
 
 let map;
 
-const init = (data, filteredData) => {
-  const center = Object.values(coordinates)
-    .reduce((acc, arr) => [acc[0] + arr[0], acc[1] + arr[1]])
-    .map(x => x / Object.keys(coordinates).length);
-  console.log([center[1], center[0]]);
-
-  const color = d3v4.scaleOrdinal(d3v4.schemeCategory10);
-const colors = Object.assign(
-  {},
-  ...Object.keys(coordinates)
-  .map(k => ({[k]: color(k)}))
-);
-colors.defaultFill = '#ABDDA4';
-console.log(colors);
-
-  map = new Datamap({
-    element: document.getElementById('mapChart'),
-    height: 1000,
-    width: 1000,
-    setProjection: function(element) {
-    var projection = d3.geo.equirectangular()
-      .center([center[1], center[0]])
-      .scale(1800);
-
-    var path = d3.geo.path()
-      .projection(projection);
-
-    return {path: path, projection: projection};
-  },
-    geographyConfig: {
-      popupOnHover: false,
-      highlightOnHover: false,
-    },
-    fills: colors,
-  });
-
-  update(data, filteredData)
-};
-
 const update = (data, filteredData) => {
   const hist = histogram(data, filteredData, 'Registration District', x => x);
-  console.log(hist);
 
   const bubbleData = hist.map(elem => ({
     name: elem.key,
@@ -70,10 +29,46 @@ const update = (data, filteredData) => {
     longitude: coordinates[elem.key][1],
     fillKey: elem.key,
   }));
-  console.log(bubbleData);
 
   map.bubbles(bubbleData);
 };
 
+const init = (data, filteredData) => {
+  const center = Object.values(coordinates)
+    .reduce((acc, arr) => [acc[0] + arr[0], acc[1] + arr[1]])
+    .map(x => x / Object.keys(coordinates).length);
+
+  const color = d3v4.scaleOrdinal(d3v4.schemeCategory10);
+  const fills = Object.assign(
+    {},
+    ...Object.keys(coordinates)
+      .map(k => ({ [k]: color(k) })),
+  );
+  fills.defaultFill = '#ABDDA4';
+
+  map = new Datamap({
+    element: document.getElementById('mapChart'),
+    height: 1000,
+    width: 1000,
+    setProjection(element) { // eslint-disable-line
+      const projection = d3.geo.equirectangular() // eslint-disable-line
+        .center([center[1], center[0]])
+        .scale(1800);
+
+      const path = d3.geo.path() // eslint-disable-line
+        .projection(projection);
+
+      return { path, projection };
+    },
+    geographyConfig: {
+      popupOnHover: false,
+      highlightOnHover: false,
+    },
+    fills,
+  });
+
+  update(data, filteredData);
+};
+
+
 export { init, update };
-/* eslint-enable */
